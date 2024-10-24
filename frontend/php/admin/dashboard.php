2f80139c0c506acdf5ed47d72c84a4a5c31263ad
DCRomero1 <det_nott@hotmail.com>
2024-10-23 21:33:37 -0500

    Cambios en el dashboards

--- frontend/php/admin/dashboard.php
+++ frontend/php/admin/dashboard.php
@@ -6,11 +6,65 @@
 <?php include("../../include/sidebar.php"); ?>
 
 
-        <!-- CONTENIDO PRINCIPAL -->
-        <main class="main-principal">
-            
-        </main>
-        <!-- CONTENIDO PRINCIPAL -->
+<!-- CONTENIDO PRINCIPAL -->       
+<main class="main-principal">
+    <div class="row">
+        <!-- Total de usuarios -->
+        <div class="col-xl-3 col-md-6">
+            <div class="card bg-primary">
+                <div class="card-body d-flex justify-content-between align-items-center text-white">
+                    <span>Total de usuarios</span>
+                    <i class="fas fa-user fa-2x"></i>
+                </div>
+                <div class="card-footer text-white">
+                    <a href="#" class="text-white">Ver detalles</a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Total de pedidos -->
+        <div class="col-xl-3 col-md-6">
+            <div class="card bg-success">
+                <div class="card-body d-flex justify-content-between align-items-center text-white">
+                    <span>Total de pedidos</span>
+                    <i class="fas fa-shopping-cart fa-2x"></i>
+                </div>
+                <div class="card-footer text-white">
+                    <a href="#" class="text-white">Ver detalles</a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Total de ventas -->
+        <div class="col-xl-3 col-md-6">
+            <div class="card bg-warning">
+                <div class="card-body d-flex justify-content-between align-items-center text-white">
+                    <span>Total de ventas</span>
+                    <i class="fas fa-dollar-sign fa-2x"></i>
+                </div>
+                <div class="card-footer text-white">
+                    <a href="#" class="text-white">Ver detalles</a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Total de pendientes -->
+        <div class="col-xl-3 col-md-6">
+            <div class="card bg-danger">
+                <div class="card-body d-flex justify-content-between align-items-center text-white">
+                    <span>Total de pendientes</span>
+                    <i class="fas fa-exclamation-circle fa-2x"></i>
+                </div>
+                <div class="card-footer text-white">
+                    <a href="#" class="text-white">Ver detalles</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<!-- CONTENIDO PRINCIPAL -->
+
+        
 
 <!-- JQUERY -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
