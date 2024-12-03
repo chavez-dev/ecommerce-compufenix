f1d0d3d9df813608bed642a5176d36b1bbf1d228
Cristhian Chávez <132727091+chavez-dev@users.noreply.github.com>
2024-12-03 00:40:09 -0500

    Update

--- backend/config/conexion.php
+++ backend/config/conexion.php
@@ -5,7 +5,7 @@ $username = "root";
 $password = "";
 //$namebd = "compufenix";
 
-// $conexion = mysqli_connect($servername, $username, $password, $namebd);
+// $conexion = mysqli_connect($servername, $username, $password, $namebd)
 $conexion = new PDO('mysql:host=junction.proxy.rlwy.net:54743;dbname=railway', $username, $password);
 
 // Verificamos si la conexion se ha establecido
